a419e27b8b1316d01a9ba64ad32dd790795dd66c
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-17 14:04:31 -0400

    changed decimal to float

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -22,10 +22,10 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.FLOAT,
       allowNull: false,
       validate: {
-        isDecimal: true,
+        isFloat: true,
       },
     },
     stock: {
